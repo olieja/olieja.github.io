b1ed1d19482be226d1399b440732c6154d7f9f8b
Hunter Braun <hunter.braun@gmail.com>
2023-12-19 20:05:58 -0600

    Initialize repo

---
+++ js/custom_behaviors.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+		//     // VARIABLES
+		// var objectArray = $(".container").find(".thumbbox");
+		//
+		//
+		// // EVENT HANDLERS
+		// $(".thumbbox").hover(
+		// 	function () {
+		// 		highlightObjectOfInterest(this)
+		// 	},
+		// 	function (){
+		// 		resetObjects()
+		// 	}
+		//   );
+		//
+		//   // FUNCTIONS
+		//   function highlightObjectOfInterest(objectOfInterest){
+		// 	for (i=0;i<objectArray.length;i++) {
+		// 		if(objectArray[i] == objectOfInterest){
+		// 			$(objectArray[i]).stop(true);
+		// 			$(objectArray[i]).fadeTo(200, .2);
+		// 		} else {
+		// 			$(objectArray[i]).stop(true);
+		// 			$(objectArray[i]).fadeTo(200, 1);
+		// 		};
+		// 	};
+		// }
+		//
+		//
+		//
+		// function resetObjects(){
+		// 	for (i=0;i<objectArray.length;i++) {
+		// 		$(objectArray[i]).stop(true);
+		// 		$(objectArray[i]).delay(100).fadeTo(200, 1);
+		// 	};
+		// };
+		
+});
